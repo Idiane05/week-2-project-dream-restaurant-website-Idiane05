ac236bb20b864b1f58ad1c20aceef34a32c17975
Idiane05 <d.ingabire1@alustudent.com>
2024-09-16 12:16:01 +0200

    feat/feat

---
+++ script.js
@@ -0,0 +1,9 @@
+ocument.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            section.classList.add('visible');
+        }
+    });
+});
